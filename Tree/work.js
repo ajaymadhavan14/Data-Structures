8cc36bc5dbdc86157effb0c647a0f791dd985fba
ajaymadhavan14 <ajay141297@gmail.com>
2023-03-31 14:29:27 +0530

    insertion work

---
+++ Tree/work.js
@@ -0,0 +1,66 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+  isEmpty() {
+    return this.root === null;
+  }
+
+  insert(value) {
+    const newNode = new Node(value);
+    if (this.isEmpty()) {
+      this.root = newNode;
+    } else {
+      this.insertNode(this.root, newNode);
+    }
+  }
+  insertNode(root, newNode) {
+    if (root.value > newNode.value) {
+      if (root.left === null) {
+        root.left = newNode;
+      } else {
+        this.insertNode(root.left, newNode);
+      }
+    } else {
+      if (root.right === null) {
+        root.right = newNode;
+      } else {
+        this.insertNode(root.right, newNode);
+      }
+    }
+  }
+
+  search(root, value) {
+    if (!root) {
+      return false;
+    } else {
+      if (root.value === value) {
+        return true;
+      } else if (root.value > value) {
+        return this.search(root.left, value);
+      } else {
+        return this.search(root.right, value);
+      }
+    }
+  }
+}
+
+const bst = new BinarySearchTree()
+bst.insert(10)
+bst.insert(6)
+bst.insert(11)
+bst.insert(8)
+bst.insert(7)
+bst.insert(12)
+bst.insert(4)
+
+console.log(bst);
+console.log(bst.root.left);
