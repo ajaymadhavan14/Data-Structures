6d3376a76d5f1c892317e94b3500a893e8a3aca4
ajaymadhavan14 <ajay141297@gmail.com>
2023-06-09 20:41:26 +0530

    DS practice

--- Toi.js
+++ Toi.js
@@ -119,3 +119,41 @@ let filterAgeReduce = users.reduce((acc,curr)=>{
   return acc
 },[])
 console.log(filterAgeReduce);
+
+
+
+let array = [1,2,1,3,4,2,5]
+
+for (let i = 0; i < array.length; i++) {
+  let count = 0
+    for (let j = 0; j < array.length; j++) {
+        if(array[i]==array[j]){
+        count++
+        }
+       
+    }
+
+    if(count==1){
+        console.log(array[i]);
+        break;
+    }
+    
+}
+
+//Riyas Toi 
+function find(n) {
+    let arr2 = [1,2,4,5,6]
+
+    for (let i = 0; i < arr2.length; i++) {
+        if(arr2[i] === n){
+            return i
+        }else{
+            if(arr2[i]>n){
+                return i
+            }
+        }
+    }
+}
+
+console.log(find(3));
+
