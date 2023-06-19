38f435f7565c9bb4a17492bf6b1b931539f2f155
ajaymadhavan14 <ajay141297@gmail.com>
2023-06-19 21:38:31 +0530

    Boading Week

---
+++ BW.js
@@ -0,0 +1,56 @@
+//Convert array to a linked list 
+
+class Node{
+    constructor(data){
+        this.data = data
+        this.next = null
+    }
+}
+
+function arrayLink(arr){
+if(arr.length === 0){
+    return null
+}
+let head = new Node(arr[0])
+let current = head
+
+for (let i = 1; i < arr.length; i++) {
+    let newNode = new Node(arr[i])
+    current.next = newNode
+    current = newNode
+}
+return head
+}
+
+let arr = [1,2,3,4,5]
+const link = arrayLink(arr)
+console.log(link);
+
+//Binery Search
+
+function BS(arr,target) {
+        let leftIndex = 0
+        let rightIndex = arr.length-1
+
+        while (leftIndex<=rightIndex) {
+            let middelIndex = Math.floor((leftIndex+rightIndex)/2)
+            if(target === arr[middelIndex]){
+                return middelIndex
+            }
+
+            if(target<arr[middelIndex]){
+                rightIndex = middelIndex-1
+            }else{
+                leftIndex = middelIndex+1
+            }
+            
+        }
+        return null
+
+
+}
+
+let array = [1,2,3,4,5,6]
+
+const findT = BS(array,2)
+console.log(findT);
