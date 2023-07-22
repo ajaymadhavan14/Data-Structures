83a0ab35399800b0df62e283f0fb0bfa1ef1e56e
ajaymadhavan14 <ajay141297@gmail.com>
2023-07-22 21:33:49 +0530

    js

---
+++ MDB.js
@@ -0,0 +1,22 @@
+var num = 8
+var num = 10
+
+console.log(num);
+
+let x = [1,2,3]
+let y = x.join("-")
+let z = y.replaceAll("-","")
+
+console.log(z);
+
+let a = 2
+let b = '2'
+
+let c = [a+b,a-b]
+
+console.log(c);
+
+
+let xx = 0
+console.log(Boolean(xx));
+console.log(Boolean(!xx));
