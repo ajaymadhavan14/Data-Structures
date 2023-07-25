539b5a0d49fe772ec181f0987136fc8877069643
ajaymadhavan14 <ajay141297@gmail.com>
2023-07-25 20:05:46 +0530

    string practice

---
+++ String/s.js
@@ -0,0 +1,22 @@
+console.log`hello`
+
+let a = {name:"ajay"}
+
+console.log(String(a));
+console.log(JSON.stringify(a));
+
+// Palindrome
+let ab = 'MALAYALAM'
+
+let ba = ab.split('').reverse().join('')
+console.log(ba);
+
+console.log(ab == ba);
+
+
+//Anagram
+
+let az = "elbowe"
+let ay = "belowe"
+
+console.log(az.split('').sort().join('') === ay.split('').sort().join(''));
