035612dd12bdd80804232404a0b4eb874ca3644c
ajaymadhavan14 <ajay141297@gmail.com>
2023-08-09 17:11:29 +0530

    js

--- js/base.js
+++ js/base.js
@@ -2,3 +2,7 @@ console.log("A + B = "+(0 || 1));
 console.log("A + B = "+(1 || 2));
 console.log("A && B = "+(0 && 1));
 console.log("A || B = "+(1 && 2));
+
+
+console.log(('b' + 'a' + + 'a' + 'a').toLowerCase());
+console.log(+'a');
