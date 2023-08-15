bb12d565c122a28b8aabfb0d8b81c8acfd1f4461
ajaymadhavan14 <ajay141297@gmail.com>
2023-08-15 19:10:10 +0530

    js

--- js/Toi.js
+++ js/Toi.js
@@ -3,3 +3,9 @@ console.log(0 == true);
 console.log(1 == true);
 
 console.log(2 == true);
+
+
+const text = "how    are    you";
+const compressedText = text.replace(/\s+/g, ' ');
+
+console.log(compressedText);  // Output: "how are you"
